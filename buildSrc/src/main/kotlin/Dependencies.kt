d3bded99b53c6d0ad7f5d4b2a4017f242bad8628
Denys <dentor2005@gmail.com>
2021-12-14 20:35:04 +0100

    Add BuildSrc Dependencies to git

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,19 @@
+@file:Suppress("unused")
+
+import dependencies.*
+
+object Dependencies {
+    val base = Base
+    val hilt = Hilt
+    val navigation = Navigation
+    val lifecycle = LifeCycle
+    val viewBindingDelegate = ViewBindingDelegate
+    val gson = Gson
+    val room = Room
+    val coroutines = Coroutines
+
+    val workManager = WorkManager
+    val retrofit = Retrofit
+    val timber = Timber
+    val glide = Glide
+}
