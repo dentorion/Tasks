942c80e2dca65d31ff1107cbb0796f7d9702105d
Denys <dentor2005@gmail.com>
2021-12-14 20:35:04 +0100

    Add BuildSrc Dependencies to git

---
+++ buildSrc/src/main/kotlin/Android.kt
@@ -0,0 +1,21 @@
+@file:Suppress("unused")
+
+object Android {
+
+    /**
+     * Build.Gradle.kts Module
+     */
+    const val buildTools = "30.0.3"
+    const val minSdk = 24
+    const val targetSdk = 31
+    const val compileSdk = 31
+
+    /**
+     * Build.Gradle.kts Project
+     */
+    const val kotlinVersion = "1.5.31"
+    const val gradleVersion = "7.1.0-beta04"
+    const val hiltVersion = "2.40"
+    const val navigation = "2.3.5"
+    const val serialization = "1.5.10"
+}
