3a415ef90cd7493cc970efb4caec80b7b7df982b
dentor2005@gmail.com <dentor2005@gmail.com>
2023-09-15 11:54:19 +0200

    Programmatically change style for snackbar.

--- app/src/main/java/com/entin/lighttasks/presentation/util/GetSnackBar.kt
+++ app/src/main/java/com/entin/lighttasks/presentation/util/GetSnackBar.kt
@@ -1,11 +1,14 @@
 package com.entin.lighttasks.presentation.util
 
 import android.view.View
+import com.entin.lighttasks.R
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
 fun getSnackBar(str: String, view: View, length: Int = Snackbar.LENGTH_SHORT): Snackbar {
     return Snackbar
         .make(view, str, length)
+        .setBackgroundTint(view.context.getColor(R.color.color_main_light_extra))
+        .setTextColor(view.context.getColor(R.color.color_text))
         .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE)
 }
